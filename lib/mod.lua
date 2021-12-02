3cf9754161226841ca6e369fadb4d9d518e8345c
Adwerd Replay <62962850+Syntheist@users.noreply.github.com>
2021-12-02 15:15:12 +0000

    1st attempt to us norns mod system with midigrid

--- lib/mod.lua
+++ lib/mod.lua
@@ -13,10 +13,7 @@ local mod = require 'core/mods'
 -- here a single table is used to hold some x/y values
 --
 
-local state = {
-  x = 0,
-  y = 0,
-}
+local grid = util.file_exists(_path.code.."midigrid") and include "midigrid/lib/mg_128" or grid
 
 
 --
@@ -35,73 +32,3 @@ local state = {
 mod.hook.register("system_post_startup", "my startup hacks", function()
   state.system_post_startup = true
 end)
-
-mod.hook.register("script_pre_init", "my init hacks", function()
-  -- tweak global environment here ahead of the script `init()` function being called
-end)
-
-
---
--- [optional] menu: extending the menu system is done by creating a table with
--- all the required menu functions defined.
---
-
-local m = {}
-
-m.key = function(n, z)
-  if n == 2 and z == 1 then
-    -- return to the mod selection menu
-    mod.menu.exit()
-  end
-end
-
-m.enc = function(n, d)
-  if n == 2 then state.x = state.x + d
-  elseif n == 3 then state.y = state.y + d end
-  -- tell the menu system to redraw, which in turn calls the mod's menu redraw
-  -- function
-  mod.menu.redraw()
-end
-
-m.redraw = function()
-  screen.clear()
-  screen.move(64,40)
-  screen.text_center(state.x .. "/" .. state.y)
-  screen.update()
-end
-
-m.init = function() end -- on menu entry, ie, if you wanted to start timers
-m.deinit = function() end -- on menu exit
-
--- register the mod menu
---
--- NOTE: `mod.this_name` is a convienence variable which will be set to the name
--- of the mod which is being loaded. in order for the menu to work it must be
--- registered with a name which matches the name of the mod in the dust folder.
---
-mod.menu.register(mod.this_name, m)
-
-
---
--- [optional] returning a value from the module allows the mod to provide
--- library functionality to scripts via the normal lua `require` function.
---
--- NOTE: it is important for scripts to use `require` to load mod functionality
--- instead of the norns specific `include` function. using `require` ensures
--- that only one copy of the mod is loaded. if a script were to use `include`
--- new copies of the menu, hook functions, and state would be loaded replacing
--- the previous registered functions/menu each time a script was run.
---
--- here we provide a single function which allows a script to get the mod's
--- state table. using this in a script would look like:
---
--- local mod = require 'name_of_mod/lib/mod'
--- local the_state = mod.get_state()
---
-local api = {}
-
-api.get_state = function()
-  return state
-end
-
-return api
