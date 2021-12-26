3fd915f224c1fac22c25367be16448f06ecff206
Adwerd Replay <62962850+Syntheist@users.noreply.github.com>
2021-12-26 13:38:25 +0000

    change to pre init

--- lib/mod.lua
+++ lib/mod.lua
@@ -32,11 +32,12 @@ util.file_exists(_path.code.."midigrid") and include "midigrid/lib/mg_128" or gr
 --
 
 mod.hook.register("system_post_startup", "my startup hacks", function()
-  grid.system_post_startup = true
+  grid.system_post_startup = false
 end)
 
 mod.hook.register("script_pre_init", "my init hacks", function()
   -- tweak global environment here ahead of the script `init()` function being called
+    grid.script_pre_init = true
 end)
 
 
